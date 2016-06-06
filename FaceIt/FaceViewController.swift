ed9bf9bc9a159e5cfa8e8197d761faed78a708ff
hugofalkman <zilla@hs-macbook-pro.local>
2016-06-06 11:45:50 +0200

    FaceIt created

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FaceViewController.swift
+//  FaceIt
+//
+//  Created by H Hugo Falkman on 2016-06-06.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
